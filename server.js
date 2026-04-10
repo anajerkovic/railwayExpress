5574b5426e8357b24a2039bff19d5858f944d244
ana.jerkovic <anajerkovic3@gmail.com>
2026-04-10 11:55:00 +0200

    Add comments for explanations

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const app = express();
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
     res.send('Pozdrav sa Railway servera!');
     });
 
+// Ruta za prikaz slika
 app.get('/slike', (req,res) => {
+    // Učitavanje slika iz JSON datoteke
     const images = JSON.parse(fs.readFileSync('images.json', 'utf8'));
     res.render("slike.ejs", {
         images: images,
